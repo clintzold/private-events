381260e01596b28b9c6d0530651a71e9798b41ed
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-11-04 09:47:01 -0700

    Add new event form page and routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,29 @@
 class EventsController < ApplicationController
+  before_action :event_params, only: [ :create ]
   def index
     @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.events.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :location, :date ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get "users/show"
   devise_for :users
   root to: "events#index"
+
   get "events", to: "events#index"
+  get "events/new", to: "events#new"
+  post "events", to: "events#create"
+  get "events/:id", to: "events#show", as: "event"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
