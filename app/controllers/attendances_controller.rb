e659db70c8a90cb4d4d2503eb208c7a9eb87b696
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-11-05 08:51:46 -0700

    fix join button displaying as invitation accept

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,14 @@
 class AttendancesController < ApplicationController
   before_action :authenticate_user!
+
   def join
     @event = Event.find(params[:event_id])
     @attendance = current_user.attendances.new(attended_event_id: @event.id)
     if @attendance.save
-      redirect_to @event, notice: "You're on the list. See you at #{@event.location}!"
+      if current_user.invitations.exists?(invited_to_id: @event.id)
+        current_user.invitations.find_by(invited_to_id: @event.id).delete
+      end
+      redirect_to my_events_path, notice: "You're on the list. See you at #{@event.location}!"
     else
       render @event, notice: "Sorry, there was an error!"
     end
@@ -14,6 +18,6 @@ class AttendancesController < ApplicationController
     @event = Event.find(params[:event_id])
     @attendance = Attendance.find(params[:attendance_id])
     @attendance.destroy
-    redirect_to @event, notice: "You have been successfully removed from the list!"
+    redirect_to my_events_path, notice: "You have been successfully removed from the list!"
   end
 end
